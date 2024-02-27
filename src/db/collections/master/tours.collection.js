d613f0d543812b83afb153e25e9cd52378da4b0a
Ali TEK <alitek@gmail.com>
2024-02-27 17:40:03 +0300

    updated

--- src/db/collections/master/tours.collection.js
+++ src/db/collections/master/tours.collection.js
@@ -2,34 +2,35 @@ const collectionName = path.basename(__filename, '.collection.js')
 module.exports = function (dbModel) {
 	let schema = mongoose.Schema(
 		{
-			owner: { type: mongoose.Schema.Types.ObjectId, ref: 'members',default:null, index: true },
+			owner: { type: mongoose.Schema.Types.ObjectId, ref: 'members', default: null, index: true },
 			title: { type: String, required: true, unique: true },
+			destination: { type: String, default: '', index: true },
 			description: { type: String, default: '' },
 			duration: { type: Number, default: 0, index: true },
 			places: { type: String, default: '' },
 			inclusions: { type: String, default: '' },
 			exclusions: { type: String, default: '' },
 			currency: { type: String, default: 'USD', enum: ['USD', 'EUR', 'AZN', 'RUB', 'TRY', 'GBP'] },
-			priceWithoutDiscount: { type: Number, default: 0, min:0 },
-			price: { type: Number, default: 0, min:0, index:true },
+			priceWithoutDiscount: { type: Number, default: 0, min: 0 },
+			price: { type: Number, default: 0, min: 0, index: true },
 			singleSupplement: { type: Number, default: 0 },
 			publishStart: { type: Date, default: Date.now, index: true },
 			publishEnd: { type: Date, default: Date.now, index: true },
 			images: [{
-        title: { type: String, default: '' },
-        src: { type: String, default: '' },
-        width: { type: Number, default: 800 },
-        height: { type: Number, default: 800 },
-        style: { type: String, default: '' },
-        alt: { type: String, default: '' },
-        thumbnail: { type: String, default: '' },
-      }],
+				title: { type: String, default: '' },
+				src: { type: String, default: '' },
+				width: { type: Number, default: 800 },
+				height: { type: Number, default: 800 },
+				style: { type: String, default: '' },
+				alt: { type: String, default: '' },
+				thumbnail: { type: String, default: '' },
+			}],
 			priceTable: [{
 				dateFrom: { type: Date, default: null },
 				dateTo: { type: Date, default: null },
 				deadline: { type: Date, default: null },
 				status: { type: String, default: '', enum: ['', 'avail', 'closed', 'cancelled'] },
-				price: { type: Number, default: 0, min:0 },
+				price: { type: Number, default: 0, min: 0 },
 			}],
 			travelPlan: [{
 				step: { type: Number, default: 0 },
@@ -40,13 +41,29 @@ module.exports = function (dbModel) {
 			passive: { type: Boolean, default: false, index: true },
 			createdDate: { type: Date, default: Date.now },
 			modifiedDate: { type: Date, default: Date.now, index: true },
+			i18n: {
+				type: Object,
+				default: {
+					lastTranslated: { type: Date, default: Date.UTC(0, 0, 0), index: true },
+					en: { title: String, description: String },
+					tr: { title: String, description: String },
+					ru: { title: String, description: String },
+					de: { title: String, description: String },
+					es: { title: String, description: String },
+					ko: { title: String, description: String },
+					fr: { title: String, description: String },
+					zh: { title: String, description: String },
+				},
+				select: false
+			},
+			temp: {}
 		},
 		{ versionKey: false }
 	)
 
-	schema.pre('save', (next) =>{ 
-		if(this.price>this.priceWithoutDiscount){
-			this.priceWithoutDiscount=this.price
+	schema.pre('save', (next) => {
+		if (this.price > this.priceWithoutDiscount) {
+			this.priceWithoutDiscount = this.price
 		}
 		next()
 	})
